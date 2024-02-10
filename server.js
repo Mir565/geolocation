d7ba4102252313b1a30fdaf486bf75bb9cdd0923
Mirjahon <mirjahonqayimov@gmail.com>
2024-02-10 05:04:08 +0500

    asasdas

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const fs = require('fs'); // Import the fs module
+app.use(express.static('public'))
 
-app.use(express.static('public'));
-
+app.set('view engine', 'ejs');
+app.get('/',(req,res)=>{
+    res.render('index.ejs')
+})
 app.get('/location', (req, res) => {
     const { latitude, longitude } = req.query;
     console.log(`Received location data - Latitude: ${latitude}, Longitude: ${longitude}`);
